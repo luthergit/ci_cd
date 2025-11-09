382c9321d7cefc362ad237870e2f43ce3c325c0c
luthergit <luthereze@yahoo.co.uk>
2025-11-09 19:29:45 +0000

    added email notifications

--- config.py
+++ config.py
@@ -1,1 +1,11 @@
-PULL_URL = "https://github.com/luthergit/chatbot.git"
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SMTP_HOST = os.getenv("SMTP_HOST")
+SMTP_PORT = int(os.getenv("SMTP_PORT"))
+SMTP_USER = os.getenv("SMTP_USER")
+SMTP_PASSWORD = os.getenv("SMTP_PASSWORD")
+ALERT_FROM = os.getenv("ALERT_FROM")
+ALERT_TO = os.getenv("ALERT_TO")
--- main.py
+++ main.py
@@ -1,12 +1,27 @@
-from fastapi import FastAPI, Request, BackgroundTasks
-from config import PULL_URL
-from pathlib import Path
 import os
 import subprocess
+import smtplib
+import traceback
+
+from fastapi import FastAPI, Request, BackgroundTasks
+from pathlib import Path
+from config import SMTP_HOST, SMTP_PORT, SMTP_USER, SMTP_PASSWORD, ALERT_FROM, ALERT_TO
+from email.message import EmailMessage
 
 
 app = FastAPI()
 
+def send_email(subject: str, body: str):
+    msg = EmailMessage()
+    msg['Subject'] = subject
+    msg['From'] = ALERT_FROM
+    msg['To'] = ALERT_TO
+    msg.set_content(body)
+    with smtplib.SMTP(SMTP_HOST, SMTP_PORT) as server:
+        server.starttls()
+        if SMTP_USER and SMTP_PASSWORD:
+            server.login(SMTP_USER, SMTP_PASSWORD)
+        server.send_message(msg)
 
 def pull_repo(project_name, clone_url):
     project_dir = Path.home()  
@@ -44,10 +59,18 @@ def redeploy():
     subprocess.run(["docker", "compose", "up", "-d"])
 
 def run_pipeline(repo_name:str, clone_url: str):
-    pull_repo(repo_name, clone_url)
-    build_image()
-    docker_down()
-    redeploy()
+    try:
+        pull_repo(repo_name, clone_url)
+        build_image()
+        docker_down()
+        redeploy()
+
+    except Exception:
+        error = traceback.format_exc()
+        send_email(
+            subject=f"Error in CI/CD pipeline for {repo_name}",
+            body=f"Repository: {repo_name}\nClone URL: {clone_url}\n\nTraceback:\n{error}"
+        )
 
 @app.post("/webhook")
 async def webhook(request: Request, background_tasks: BackgroundTasks):
@@ -57,7 +80,7 @@ async def webhook(request: Request, background_tasks: BackgroundTasks):
     clone_url = payload['repository']['clone_url']
     background_tasks.add_task(run_pipeline, repo_name, clone_url)
 
-    return {"message": "Webhook received and pipelinescheduled"}
+    return {"message": "Webhook received and pipeline scheduled"}
 
 @app.get("/")
 def home():
